fd35e0cc8bcf1a21df44bb071adfb0167e4cb0f7
Kurchaviy Vitaliy <vitalykurchaviy@yandex.ru>
2024-07-11 18:50:33 +0300

    add database model tdocuments

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Numeric
+
+
+Base = declarative_base()
+
+
+class TDocuments(Base):
+    __tablename__ = 'text_documents'
+    url = Column(String, primary_key=True)
+    pub_date = Column(Numeric(20, 0), nullable=False)
+    fetch_time = Column(Numeric(20, 0), nullable=False)
+    text = Column(String, nullable=False)
+    first_fetch_time = Column(Numeric(20, 0), nullable=False)
