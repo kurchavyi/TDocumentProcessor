a4d6e439dec4c930418d43202de3502bf0625065
Kurchaviy Vitaliy <vitalykurchaviy@yandex.ru>
2024-07-13 16:02:38 +0300

    change the structure

--- documents_processor.py
+++ documents_processor.py
@@ -14,51 +14,43 @@ class DocumentsProcessor:
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
 
-    def process_document(self, url, pub_date, fetch_time, text) -> None:
+    def process_document(self, dict_document) -> None:
         """
             process document
         """
-        document = self.get(url=url)
-        if document:
-            if fetch_time < document.first_fetch_time:
-                document.first_fetch_time = fetch_time
-                document.pub_date = pub_date
-            if fetch_time > document.fetch_time:
-                document.fetch_time = fetch_time
-                document.text = text
-            self.session.commit()
+        new_document = TDocuments.from_dict(dict_document)
+        old_document = self.get(url=new_document.url)
+        if old_document:
+            result_document = self.__update(old_document=old_document, new_document=new_document)
         else:
-            document = TDocuments(
-                url=url,
-                pub_date=pub_date,
-                fetch_time=fetch_time,
-                text=text,
-                first_fetch_time=fetch_time
-        )
-            self.__insert(document)
-        return document
+            result_document = self.__insert(new_document)
+        return result_document
 
 
-    def __insert(self, new_document) -> None:
+    def __insert(self, new_document: TDocuments) -> TDocuments:
         """
             Add document in database.
         """
+        new_document.first_fetch_time = new_document.fetch_time
         self.session.add(new_document)
         self.session.commit()
         return new_document
 
 
-    def __update(self, document, url, pub_date, fetch_time, text, first_fetch_time) -> None:
+    def __update(self, old_document : TDocuments, new_document: TDocuments) -> TDocuments:
         """
             Update document in database.
         """
-        document.pub_date = pub_date
-        document.fetch_time = fetch_time
-        document.text = text
-        document.first_fetch_time = first_fetch_time
+        if new_document.fetch_time < old_document.first_fetch_time:
+            old_document.first_fetch_time = new_document.fetch_time
+            old_document.pub_date = new_document.pub_date
+        if new_document.fetch_time > old_document.fetch_time:
+            old_document.fetch_time = new_document.fetch_time
+            old_document.text = new_document.text
         self.session.commit()
+        return old_document
 
-    def get(self, url):
+    def get(self, url: str) -> TDocuments:
         """
             Get document from database by given url.
             If document not exist return None.
