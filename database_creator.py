4a5fcb0308c1375918b293564a6f3bf8652b6282
Kurchaviy Vitaliy <vitalykurchaviy@yandex.ru>
2024-07-15 10:45:39 +0300

    cast to common style

--- database_creator.py
+++ database_creator.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from models import TDocuments, Base
 import os
 
+
 def get_database_path():
     load_dotenv()
     DBUSERNAME = os.getenv("DBUSERNAME")
@@ -11,8 +12,18 @@ def get_database_path():
     PASSWORD = os.getenv("PASSWORD")
     LOCALHOST = os.getenv("LOCALHOST")
     DATABASE = os.getenv("DATABASE")
-    DATABASE_URL = "postgresql://" + DBUSERNAME + ":" + PASSWORD + "@localhost:" + LOCALHOST + "/" + DATABASE
+    DATABASE_URL = (
+        "postgresql://"
+        + DBUSERNAME
+        + ":"
+        + PASSWORD
+        + "@localhost:"
+        + LOCALHOST
+        + "/"
+        + DATABASE
+    )
     return DATABASE_URL
+
 
 if __name__ == "__main__":
     DATABASE_URL = get_database_path()
@@ -24,9 +35,27 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    doc1 = TDocuments(url="1", pub_date=20, fetch_time=25, text='url="1", pub_date=20, fetch_time=25', first_fetch_time=25)
-    doc2 = TDocuments(url="2", pub_date=30, fetch_time=40, text='url="2", pub_date=30, fetch_time=40', first_fetch_time=30)
-    doc3 = TDocuments(url="3", pub_date=30, fetch_time=45, text='url="3", pub_date=30, fetch_time=45', first_fetch_time=45)
+    doc1 = TDocuments(
+        url="1",
+        pub_date=20,
+        fetch_time=25,
+        text='url="1", pub_date=20, fetch_time=25',
+        first_fetch_time=25,
+    )
+    doc2 = TDocuments(
+        url="2",
+        pub_date=30,
+        fetch_time=40,
+        text='url="2", pub_date=30, fetch_time=40',
+        first_fetch_time=30,
+    )
+    doc3 = TDocuments(
+        url="3",
+        pub_date=30,
+        fetch_time=45,
+        text='url="3", pub_date=30, fetch_time=45',
+        first_fetch_time=45,
+    )
 
     session.add(doc1)
     session.add(doc2)
@@ -41,6 +70,6 @@ if __name__ == "__main__":
     table_names = inspector.get_table_names()
     print(f"Tables: {table_names}")
 
-    columns = inspector.get_columns('text_documents')
+    columns = inspector.get_columns("text_documents")
     for column in columns:
         print(f"Column: {column['name']}, Type: {column['type']}")
